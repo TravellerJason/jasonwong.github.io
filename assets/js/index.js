9739b0f4057e14c11c896d39b189124123f1faf6
TravellerJason <34296844ab@gmail.com>
2019-10-16 20:51:54 +0800

    modify

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
-// setting main-img-canvas(if image cannot show)
 function background_canvas()
-{
+{		// setting main-img-canvas(if image cannot show)
 	let canvas = document.getElementById("canvas");
 	let cxt = canvas.getContext("2d");
 	let gradient = cxt.createLinearGradient(0,0,0,canvas.height);
@@ -22,11 +21,44 @@ function getElementPosition(el_id)
 	}
 }
 
-// function setElementPosition_for_header(p_el_id, s_el_id)
-// {
-// 	let a = getElementPosition(p_el_id);
-// 	let b = document.getElementById(s_el_id);
-// 	b.style.position="absolute";
-// 	b.style.top = "62px";
-// 	b.style.left = a.left - a.width / 2 + "px";
-// }
+function resize()
+{
+	let screenWidth = document.body.clientWidth;
+	let screenHeight = document.body.clientHeight;
+	let heading_img = document.getElementById("home-heading-img")
+	let heading_img_height = heading_img.getBoundingClientRect().height;
+	let header = document.getElementById("header");
+	let big_header_list = document.getElementById("big-header-list");
+	let small_header_list = document.getElementById("small-header-list");
+	if(screenWidth < screenHeight || screenWidth < 800) {
+		header.classList.add("small-screen");
+		header.classList.remove("big-screen");
+
+		big_header_list.classList.add("hidden");
+		big_header_list.classList.remove("visible");
+
+		small_header_list.classList.add("visible");
+		small_header_list.classList.remove("hidden");
+	} else {
+		header.classList.add("big-screen");
+		header.classList.remove("small-screen");
+
+		big_header_list.classList.add("visible");
+		big_header_list.classList.remove("hidden");
+
+		small_header_list.classList.add("hidden");
+		small_header_list.classList.remove("visible");
+	}
+	if(screenWidth < 1.4 * screenHeight) {
+		heading_img.classList.add("small-screen");
+		heading_img.classList.remove("big-screen");
+	} else {
+		heading_img.classList.add("big-screen");
+		heading_img.classList.remove("small-screen");
+	}
+}
+
+
+
+resize();
+window.onresize = resize;
